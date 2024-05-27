23e9b17d228b0262f6b65722df7225efb140c5d5
AashishMishra <aashishmishra5131@gmail.com>
2024-05-27 15:40:23 +0530

    fix port

--- index.js
+++ index.js
@@ -9,17 +9,22 @@ dotenv.config();
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(cors()); 
 
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api', dnsRoutes);
 
+// Database connection and server start
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('MongoDB connected');
-        app.listen(process.env.PORT, () => {
-            console.log(`Server running on port ${process.env.PORT}`);
+
+        const PORT = process.env.PORT || 4000; // Use PORT from env or default to 3000
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
         });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => console.log('MongoDB connection error:', err));
